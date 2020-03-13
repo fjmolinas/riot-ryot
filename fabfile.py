37a751ece823753fa33343050cff0c3626b613c2
Francisco Molina <femolina@uc.cl>
2020-03-13 09:10:38 +0100

    fabfile: add picocom

--- fabfile.py
+++ fabfile.py
@@ -113,7 +113,7 @@ def install_common():
 @task
 def install_riot():
     """Install riot specific tools."""
-    packages = ['make', 'docker.io', 'socat']
+    packages = ['make', 'docker.io', 'socat', 'picocom']
     packages += ['python3-serial', 'python3-pexpect']
     packages += ['python3-cryptography', 'python3-pyasn1',
                  'python3-ecdsa', 'python3-crypto']
